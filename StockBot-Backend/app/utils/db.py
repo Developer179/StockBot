a1a5f1561951389f66f5873b3a99829671b606bb
shivansh179 <prasantshukla89@gmail.com>
2025-04-15 19:32:16 +0530

    changed port of rasa run actions

--- StockBot-Backend/app/utils/db.py
+++ StockBot-Backend/app/utils/db.py
@@ -10,7 +10,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_DIR = os.path.join(BASE_DIR, "..", "..", "logs")
 os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE = os.path.join(LOG_DIR, "app.log")
 
 logging.basicConfig(
     level=logging.DEBUG,
